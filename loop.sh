33a12e6a670d93d5b4b653cbe9c3c5059cfad8f2
Renzo Lucioni <renzolucioni@gmail.com>
2023-03-11 19:29:09 -0500

    fix hosts, shallower tree after testing, less wait

--- loop.sh
+++ loop.sh
@@ -7,7 +7,7 @@ OUTPUT='data.txt'
 while read -r line
 do
   date -Iseconds >> $OUTPUT
-  traceroute -q 1 -z 500 -m 64 -w 3 $line >> $OUTPUT
+  traceroute -q 1 -z 500 -m 32 -w 1 $line >> $OUTPUT
 
   echo "ok"
 
